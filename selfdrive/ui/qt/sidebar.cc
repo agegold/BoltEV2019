b666bb5dc39d41023d26ca283c78ee8c43ecfb40
agegold <66872114+agegold@users.noreply.github.com>
2023-08-31 21:03:52 +0900

    Update sidebar.cc

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -57,27 +57,29 @@ void Sidebar::updateState(const UIState &s) {
   setProperty("netStrength", strength > 0 ? strength + 1 : 0);
   setProperty("wifiAddr", deviceState.getWifiIpAddress().cStr());
 
-  ItemStatus connectStatus;
-  auto last_ping = deviceState.getLastAthenaPingTime();
-  if (last_ping == 0) {
-    connectStatus = params.getBool("PrimeRedirected") ? ItemStatus{"NO\nPRIME", danger_color} : ItemStatus{"CONNECT\nOFFLINE", warning_color};
-  } else {
-    connectStatus = nanos_since_boot() - last_ping < 80e9 ? ItemStatus{"CONNECT\nONLINE", good_color} : ItemStatus{"CONNECT\nERROR", danger_color};
-  }
-  setProperty("connectStatus", QVariant::fromValue(connectStatus));
+  int batteryPercent = deviceState.getBatteryPercent();
+  QColor batteryColor = good_color;
+  if(batteryPercent < 30)
+    batteryColor = warning_color;
+  else if(batteryPercent < 10)
+    batteryColor = danger_color;
 
-  ItemStatus tempStatus = {"TEMP\nHIGH", danger_color};
+  QString batteryDesc;
+  batteryDesc.sprintf("%d%%\nBATTERY", batteryPercent);
+  setProperty("batteryPercent", QVariant::fromValue(ItemStatus{batteryDesc, batteryColor}));
+
+  ItemStatus tempStatus = {"온도\n높음", danger_color};
   auto ts = deviceState.getThermalStatus();
   if (ts == cereal::DeviceState::ThermalStatus::GREEN) {
-    tempStatus = {"TEMP\nGOOD", good_color};
+    tempStatus = {"온도\n정상", good_color};
   } else if (ts == cereal::DeviceState::ThermalStatus::YELLOW) {
-    tempStatus = {"TEMP\nOK", warning_color};
+    tempStatus = {"온도\n정상", warning_color};
   }
   setProperty("tempStatus", QVariant::fromValue(tempStatus));
 
-  ItemStatus pandaStatus = {"VEHICLE\nONLINE", good_color};
+  ItemStatus pandaStatus = {"판다\n연결됨", good_color};
   if (s.scene.pandaType == cereal::PandaState::PandaType::UNKNOWN) {
-    pandaStatus = {"NO\nPANDA", danger_color};
+    pandaStatus = {"판다\n연결안됨", danger_color};
   } /*else if (s.scene.started && !sm["liveLocationKalman"].getLiveLocationKalman().getGpsOK()) {
     pandaStatus = {"GPS\nSEARCH", warning_color};
   }*/
@@ -120,5 +122,5 @@ void Sidebar::paintEvent(QPaintEvent *event) {
   configFont(p, "Open Sans", 35, "Regular");
   drawMetric(p, temp_status.first, temp_status.second, 338);
   drawMetric(p, panda_status.first, panda_status.second, 496);
-  drawMetric(p, connect_status.first, connect_status.second, 654);
+  drawMetric(p, battery_percent.first, battery_percent.second, 654);
 }
